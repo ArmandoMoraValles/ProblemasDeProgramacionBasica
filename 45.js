30b2e4307a953b46e933437ce040d57b2cadddc6
armandomoravalles <armandomoravalles@gmail.com>
2023-11-03 04:01:02 -0600

    45.- Tenemos el archivo DATOS.DAT con la misma estructura anterior, que esta indexado por el campo ID. Crear un programa que nos permita consultar un registro siempre que queramos.

--- 45.js
+++ 45.js
@@ -1,6 +1,6 @@
 /*
-    44.- Con el archivo DATOS.DAT listar todos los registros cuyo 
-    estado sea uno determinado que introduciremos por teclado
+    45.- Tenemos el archivo DATOS.DAT con la misma estructura anterior, que esta indexado por el
+    campo ID. Crear un programa que nos permita consultar un registro siempre que queramos.
 */
 
 const fs = require('fs');
@@ -8,25 +8,24 @@ const readline = require('readline');
 
 // Poder leer datos por teclado con node sin necesidad de usar el navegador
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-rl.question('Introduce el estado a buscar: ', (estadoABuscar) => {
+rl.question('Introduce el id a buscar: ', (estadoABuscar) => {
     fs.readFile('DATOS.DAT', 'utf8', (err, data) => {
         if (err) {
             console.error('Error al leer el archivo DATOS.DATA:', err);
         } else {
             const registros = JSON.parse(data);
-
-            console.log('Registros con estado', estadoABuscar, ':');
-        for (const registro of registros) {
-            if (registro.ESTADO === estadoABuscar) {
-                console.log(registro);
+            console.log('Registro con id', estadoABuscar, ':');
+            for (const registro of registros) {
+                if (registro.ID === estadoABuscar) {
+                    console.log(registro);
+                }
             }
-        }
-
-        rl.close();
+    
+            rl.close();
         }
     });
 });
