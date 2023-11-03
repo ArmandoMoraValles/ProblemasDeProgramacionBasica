5f24d3471a94265a7dc902cc1e3ceaaeb49aae4c
armandomoravalles <armandomoravalles@gmail.com>
2023-11-02 21:11:48 -0600

    2.- Hacer un programa que imprima los números del 100 al 0, en orden decreciente.

---
+++ 2.js
@@ -0,0 +1,6 @@
+//2.- Hacer un programa que imprima los números del 100 al 0, en orden decreciente. 
+const number = 100;
+
+for (let index = 0; index <= number; index++) {
+    console.log(number - index);
+}
