f53f2dbbd67a93140b7bc0a72c6880fd76909e54
armandomoravalles <armandomoravalles@gmail.com>
2023-11-02 23:52:46 -0600

    27.- Realizar la tabla de multiplicar de un numero entre 0 y 10.

---
+++ 27.js
@@ -0,0 +1,8 @@
+// 27.- Realizar la tabla de multiplicar de un numero entre 0 y 10. 
+
+const numero = 8;
+
+for (let i = 1; i <= 10; i++) {
+    const resultado = numero * i;
+    console.log(`${numero} . ${i} = ${resultado}`);
+}
