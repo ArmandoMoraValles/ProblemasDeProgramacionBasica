555e1b5afc8fb6fe03d3c7b9a6cd4ac751a4e62c
armandomoravalles <armandomoravalles@gmail.com>
2023-11-02 23:53:50 -0600

    28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.

---
+++ 28.js
@@ -0,0 +1,10 @@
+// 28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz. 
+
+const resultado = Math.floor(Math.random() * 2);
+  
+if (resultado === 0) {
+      console.log('Cara');
+} else {
+      console.log('Cruz');
+}
+  
